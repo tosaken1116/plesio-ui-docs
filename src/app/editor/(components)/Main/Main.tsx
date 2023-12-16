9aee9e2e1b8e45209084002042ba1a29c2b775bc
Teba_eleven <73474645+Tebaeleven@users.noreply.github.com>
2023-12-16 16:27:44 +0900

    色の修正

--- src/app/editor/(components)/Main/Main.tsx
+++ src/app/editor/(components)/Main/Main.tsx
@@ -5,7 +5,7 @@ import Check from './Check';
 const Main = () => (
   <div className="grid grid-cols-5 h-screen">
     <main className="col-span-3 py-10 bg-white drop-shadow" />
-    <div className="col-span-2 bg-slate-600 p-10">
+    <div className="col-span-2 bg-slate-800 p-10 ">
       <Check />
     </div>
   </div>
--- src/app/editor/(components)/Sidebar/Sidebar.tsx
+++ src/app/editor/(components)/Sidebar/Sidebar.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 const Sidebar = () => (
   <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
     {/* Sidebar component, swap this element with another sidebar if you like */}
-    <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-slate-600 px-6 pb-4">
+    <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-slate-800 px-6 pb-4">
       <div className="flex h-16 shrink-0 items-center">
         <img
           className="h-8 w-auto"
