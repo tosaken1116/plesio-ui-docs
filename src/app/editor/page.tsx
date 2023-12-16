fb3cdbc3a5757d58c25487b2cd1daec4096a38f7
Teba_eleven <73474645+Tebaeleven@users.noreply.github.com>
2023-12-16 13:24:36 +0900

    画面構造のリファクタリング

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -510,158 +510,23 @@ Leaving: "transition ease-in-out duration-300 transform"
         </nav>
       </div>
     </div>
-    <div className="lg:pl-72">
-      <div className="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-200 bg-white px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8">
-        <button type="button" className="-m-2.5 p-2.5 text-gray-700 lg:hidden">
-          <span className="sr-only">Open sidebar</span>
-          <svg
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            aria-hidden="true"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-            />
-          </svg>
-        </button>
-        {/* Separator */}
-        <div className="h-6 w-px bg-gray-900/10 lg:hidden" aria-hidden="true" />
-        <div className="flex flex-1 gap-x-4 self-stretch lg:gap-x-6">
-          <form className="relative flex flex-1" action="#" method="GET">
-            <label htmlFor="search-field" className="sr-only">
-              Search
-            </label>
-            <svg
-              className="pointer-events-none absolute inset-y-0 left-0 h-full w-5 text-gray-400"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              aria-hidden="true"
-            >
-              <path
-                fillRule="evenodd"
-                d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
-                clipRule="evenodd"
-              />
-            </svg>
-            <input
-              id="search-field"
-              className="block h-full w-full border-0 py-0 pl-8 pr-0 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm"
-              placeholder="Search..."
-              type="search"
-              name="search"
-            />
-          </form>
-          <div className="flex items-center gap-x-4 lg:gap-x-6">
-            <button
-              type="button"
-              className="-m-2.5 p-2.5 text-gray-400 hover:text-gray-500"
-            >
-              <span className="sr-only">View notifications</span>
-              <svg
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
-                />
-              </svg>
-            </button>
-            {/* Separator */}
-            <div
-              className="hidden lg:block lg:h-6 lg:w-px lg:bg-gray-900/10"
-              aria-hidden="true"
-            />
-            {/* Profile dropdown */}
-            <div className="relative">
-              <button
-                type="button"
-                className="-m-1.5 flex items-center p-1.5"
-                id="user-menu-button"
-                aria-expanded="false"
-                aria-haspopup="true"
-              >
-                <span className="sr-only">Open user menu</span>
-                <img
-                  className="h-8 w-8 rounded-full bg-gray-50"
-                  src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                  alt=""
-                />
-                <span className="hidden lg:flex lg:items-center">
-                  <span
-                    className="ml-4 text-sm font-semibold leading-6 text-gray-900"
-                    aria-hidden="true"
-                  >
-                    Tom Cook
-                  </span>
-                  <svg
-                    className="ml-2 h-5 w-5 text-gray-400"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                </span>
-              </button>
-              {/*
-      Dropdown menu, show/hide based on menu state.
 
-      Entering: "transition ease-out duration-100"
-        From: "transform opacity-0 scale-95"
-        To: "transform opacity-100 scale-100"
-      Leaving: "transition ease-in duration-75"
-        From: "transform opacity-100 scale-100"
-        To: "transform opacity-0 scale-95"
-    */}
-              <div
-                className="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none"
-                role="menu"
-                aria-orientation="vertical"
-                aria-labelledby="user-menu-button"
-                tabIndex={-1}
-              >
-                {/* Active: "bg-gray-50", Not Active: "" */}
-                <a
-                  href="#"
-                  className="block px-3 py-1 text-sm leading-6 text-gray-900"
-                  role="menuitem"
-                  tabIndex={-1}
-                  id="user-menu-item-0"
-                >
-                  Your profile
-                </a>
-                <a
-                  href="#"
-                  className="block px-3 py-1 text-sm leading-6 text-gray-900"
-                  role="menuitem"
-                  tabIndex={-1}
-                  id="user-menu-item-1"
-                >
-                  Sign out
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <main className="py-10">
+    <div className="lg:pl-72 h-screen flex flex-col">
+      <main className="py-10 bg-white flex-grow">
         <div className="px-4 sm:px-6 lg:px-8">{/* Your content */}</div>
       </main>
+      <footer className="py-4 bg-gray-800 text-white text-center">
+        <span className="inline-flex items-center gap-x-1.5 rounded-md px-2 py-1 text-xs font-medium text-white ring-1 ring-inset ring-gray-200">
+          <svg
+            className="h-1.5 w-1.5 fill-red-500"
+            viewBox="0 0 6 6"
+            aria-hidden="true"
+          >
+            <circle cx={3} cy={3} r={3} />
+          </svg>
+          Badge
+        </span>
+      </footer>
     </div>
   </div>
 );
