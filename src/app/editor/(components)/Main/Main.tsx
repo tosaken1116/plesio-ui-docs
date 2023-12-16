5be15cc465f976bdc551a1cc247de8817b2c0774
Teba_eleven <73474645+Tebaeleven@users.noreply.github.com>
2023-12-16 15:42:43 +0900

    色、コンポーネントなどの改良

--- src/app/editor/(components)/Main/Main.tsx
+++ src/app/editor/(components)/Main/Main.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
 const Main = () => (
-
+  <div className="grid grid-cols-5 h-screen">
+    <main className="col-span-3 py-10 bg-white" />
+    <div className="col-span-2 bg-slate-600 p-5" />
+  </div>
 );
 
 export default Main;
--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -1,26 +1,22 @@
+'use client';
+
 import React from 'react';
 
 import Footer from './(components)/Footer/Footer';
+import Main from './(components)/Main/Main';
 import Sidebar from './(components)/Sidebar/Sidebar';
 
 const Editor = (): React.JSX.Element => (
   <div>
-    {/* Static sidebar for desktop */}
+    {/* 左サイドバー */}
     <Sidebar />
 
     <div className="lg:pl-72 h-screen flex flex-col">
       {/* メイン */}
-      <div className="grid grid-cols-5 h-screen">
-        <main className="col-span-3 py-10 bg-green-300">
-          <div className="px-4 sm:px-6 lg:px-8">{/* Your content */}</div>
-        </main>
-        <div className="col-span-2 bg-red-500" />
-      </div>
+      <Main />
       {/* フッター */}
       <Footer />
     </div>
-
-    {/* 右サイドバー */}
   </div>
 );
 
