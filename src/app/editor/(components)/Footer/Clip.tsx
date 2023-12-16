633d1945d5282b2a6ecfee1fd483092380571280
Teba_eleven <73474645+Tebaeleven@users.noreply.github.com>
2023-12-16 14:21:29 +0900

    left,top指定できるように

--- src/app/editor/(components)/Footer/Clip.tsx
+++ src/app/editor/(components)/Footer/Clip.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 
-const Clip = () => {
+const Clip: React.FC = ({ x, y }: { y: number }) => {
   const width = Math.floor(Math.random() * 1500) + 150;
   return (
     <span
       className={`rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 `}
-      style={{ width: `${width}px` }}
+      style={{
+        position: 'absolute',
+        width: `${width}px`,
+        left: `${x}px`,
+        top: `${y + 15}px`,
+      }}
     >
       ボタンのテキスト
     </span>
--- src/app/editor/(components)/Footer/Footer.tsx
+++ src/app/editor/(components)/Footer/Footer.tsx
@@ -3,11 +3,12 @@ import React from 'react';
 import Clip from './Clip';
 
 const Footer = () => (
-  <footer className="py-4 bg-gray-800 text-white text-center flex flex-col items-start gap-1 p-5">
-    <Clip />
-    <Clip />
-    <Clip />
-    <Clip />
+  <footer className="py-4 bg-gray-800 text-white text-center flex flex-col items-start gap-1 p-5 relative overflow-x-scroll h-64">
+    <Clip y={0} x={Math.floor(Math.random() * 1500)} />
+    <Clip y={50} x={Math.floor(Math.random() * 1500)} />
+    <Clip y={100} x={Math.floor(Math.random() * 1500)} />
+    <Clip y={150} x={Math.floor(Math.random() * 1500)} />
+    <Clip y={200} x={Math.floor(Math.random() * 1500)} />
   </footer>
 );
 
