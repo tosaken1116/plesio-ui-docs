7c0fbc681de26de84d8125229de3547aa4be2a50
Teba_eleven <73474645+Tebaeleven@users.noreply.github.com>
2023-12-16 13:40:08 +0900

    エディタのタイムラインの試作

---
+++ src/app/editor/(components)/Footer/Clip.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Clip = () => (
+  <span className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 w-full">
+    Button text
+  </span>
+);
+
+export default Clip;
--- src/app/editor/(components)/Footer/Footer.tsx
+++ src/app/editor/(components)/Footer/Footer.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
 
+import Clip from './Clip';
+
 const Footer = () => (
-  <footer className="py-4 bg-gray-800 text-white text-center">
-    <span className="inline-flex items-center gap-x-1.5 rounded-md px-2 py-1 text-xs font-medium text-white ring-1 ring-inset ring-gray-200">
-      <svg
-        className="h-1.5 w-1.5 fill-red-500"
-        viewBox="0 0 6 6"
-        aria-hidden="true"
-      >
-        <circle cx={3} cy={3} r={3} />
-      </svg>
-      Badge
-    </span>
+  <footer className="py-4 bg-gray-800 text-white text-center flex flex-col items-center gap-1 p-5">
+    <Clip />
+    <Clip />
+    <Clip />
+    <Clip />
   </footer>
 );
 
