2df516108168619ffacaa65fb469873c4f6460af
Teba_eleven <73474645+Tebaeleven@users.noreply.github.com>
2023-12-16 23:02:12 +0900

    code ハイライト

--- src/app/editor/(components)/Main/Tabs/Code.tsx
+++ src/app/editor/(components)/Main/Tabs/Code.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
 
-function Code() {
+import dedent from 'dedent';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { tomorrow } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+
+const CodeBlock: React.FC = () => {
+  const code = dedent`
+  function add(a, b) {
+    return a + b;
+  }`;
   return (
-    <div>Code</div>
-  )
-}
+    <SyntaxHighlighter language="tsx" style={tomorrow}>
+      {code}
+    </SyntaxHighlighter>
+  );
+};
 
-export default Code
+export default CodeBlock;
