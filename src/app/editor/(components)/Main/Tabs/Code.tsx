9d121fdc02134dedf1b1112f17caca3894e65cfb
Teba_eleven <73474645+Tebaeleven@users.noreply.github.com>
2023-12-17 13:55:13 +0900

    コード自動生成

--- src/app/editor/(components)/Main/Tabs/Code.tsx
+++ src/app/editor/(components)/Main/Tabs/Code.tsx
@@ -1,10 +1,39 @@
 import React, { useState } from 'react';
 
+import dedent from 'dedent-js';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { tomorrow } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
+import { SelectedContext } from '../../Context/SelectedContext';
+
 const CodeBlock: React.FC = () => {
-  const [code, setCode] = useState<string>('');
+  const { selected } = React.useContext(SelectedContext);
+
+  const uicode = dedent`
+  import React from 'react';
+
+  export const Code = () => {
+    <Button
+      radius= '${selected.radius}',
+      outline= '${selected.outline}',
+      state= '${selected.state}',
+      animationProps={{
+        hover: {
+          key: '${selected.key}',
+          option: {
+            duration: '${selected.duration}',
+            delay: '${selected.delay}',
+            axis: '${selected.axis}',
+          },
+        },
+      }}
+    >
+      ボタン
+    </Button>;
+  };
+  
+`;
+  const [code, setCode] = useState<string>(uicode);
   return (
     <div>
       <SyntaxHighlighter language="tsx" style={tomorrow}>
