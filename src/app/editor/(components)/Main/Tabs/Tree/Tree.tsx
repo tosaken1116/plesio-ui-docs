69392339fc9ae295ce6f586d6a3afb71d24e6dd4
Teba_eleven <73474645+Tebaeleven@users.noreply.github.com>
2023-12-17 09:52:37 +0900

    treeのテスト

--- src/app/editor/(components)/Main/Tabs/Tree/Tree.tsx
+++ src/app/editor/(components)/Main/Tabs/Tree/Tree.tsx
@@ -37,24 +37,55 @@ const Tree = () => {
             name: 'child',
             children: [],
           },
+          {
+            id: 8,
+            name: 'child',
+            children: [],
+          },
+          {
+            id: 9,
+            name: 'child',
+            children: [],
+          },
         ],
       },
     ],
   };
 
+  let depth = 0;
   const renderTree = (
     node: { id: number; name: string; children: any[] },
     posX = 0,
     posY = 50
-  ): JSX.Element => (
-    <div>
-      <TreeNode x={posX} y={posY} name={node.name} />
+  ): JSX.Element => {
+    const childrenCount = node.children.length;
 
-      {node.children.map((child, index) =>
-        renderTree(child, posX + index * 100, posY + 150)
-      )}
-    </div>
-  );
+    ++depth;
+    return (
+      <div>
+        {childrenCount !== 1 ? (
+          <TreeNode
+            x={posX + (childrenCount * 50) / 3}
+            y={posY}
+            name={`${childrenCount}`}
+          />
+        ) : (
+          <TreeNode x={posX} y={posY} name={`${childrenCount}`} />
+        )}
+
+        {node.children.map((child: typeof data, index) =>
+          renderTree(
+            child,
+            posX +
+              (childrenCount > 1
+                ? (index * 200 + (childrenCount - 1) * 50) / 3
+                : index * 200),
+            posY + 150
+          )
+        )}
+      </div>
+    );
+  };
 
   return renderTree(data);
 };
