3412dc04e54fffe96c54e297dd341052151c51ca
uuronn <uuronprogram@gmail.com>
2023-12-17 16:44:28 +0900

    feat: add RadioButtonGroup animation docs

--- src/components/page/Radio/index.tsx
+++ src/components/page/Radio/index.tsx
@@ -1,1 +1,63 @@
-export const Screen: React.FC = () => <>this is page of Radio </>;
+'use client';
+
+import { RadioButtonGroup, Typography } from '@plesiosaurus/ui';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { dracula } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+
+import { ComponentProvider } from '@/components/ui/ComponentProvider';
+
+export const Screen: React.FC = () => (
+  <>
+    <div>
+      <Typography variant="h1" component="h1">
+        RadioButtonGroup
+      </Typography>
+      <div>
+        <Typography variant="h3" component="h3">
+          Default RadioButtonGroup
+        </Typography>
+        <SyntaxHighlighter language="javascript" style={dracula}>
+          {`
+          const RadioButtonGroupExample = () => (
+            <RadioButtonGroup
+              options={['example1', 'example2', 'example3']}
+              defaultValue="example1"
+            />
+          )
+          `}
+        </SyntaxHighlighter>
+
+        <ComponentProvider>
+          <RadioButtonGroup
+            options={['example1', 'example2', 'example3']}
+            defaultValue="example1"
+          />
+        </ComponentProvider>
+      </div>
+    </div>
+    <div>
+      <Typography variant="h3" component="h3">
+        RadioButtonGroup zoomIn
+      </Typography>
+      <SyntaxHighlighter language="javascript" style={dracula}>
+        {`
+          const RadioButtonGroupExample = () => (
+            <RadioButtonGroup
+              options={['example1', 'example2', 'example3']}
+              defaultValue="example1"
+              zoomIn
+            />
+          )
+          `}
+      </SyntaxHighlighter>
+
+      <ComponentProvider>
+        <RadioButtonGroup
+          options={['example1', 'example2', 'example3']}
+          defaultValue="example1"
+          zoomIn
+        />
+      </ComponentProvider>
+    </div>
+  </>
+);
